3f9d2f750aabc3f13216e2ea98b2d85d8c0feeda
Just Burrow <just.burrow@lul.kr>
2016-02-28 16:41:52 +0900

    5: 랜덤 유틸리티
    Task-Url: https://github.com/JustBurrow/urs/issues/issue/5
    
    * `int`형.
    * `long`형 일부.

---
+++ src/main/java/kr/lul/urs/util/Randoms.java
@@ -0,0 +1,282 @@
+package kr.lul.urs.util;
+
+import static java.lang.String.format;
+
+import java.math.BigInteger;
+import java.util.Random;
+
+/**
+ * 각 데이터 타입의 최대 범위를 엄밀하게 고려하지는 않지만, 대략적인 임의의 값을 만드는 유틸리티 메서드 모음.
+ *
+ * @author just.burrow@lul.kr
+ */
+public abstract class Randoms {
+  private static final Random R = new Random();
+
+  /**
+   * 임의의 <code>boolean</code>을 고른다.
+   *
+   * @return 임의의 <code>boolean</code>.
+   */
+  public static boolean bool() {
+    return R.nextBoolean();
+  }
+
+  /**
+   * 임의의 음수를 반환한다.
+   *
+   * @return 임의의 음수.
+   */
+  public static int negative() {
+    return -1 - R.nextInt(Integer.MAX_VALUE);
+  }
+
+  /**
+   * 임의의 음수를 반환한다.
+   *
+   * @return 임의의 음수.
+   */
+  public static long negativeLong() {
+    long val = R.nextLong();
+    if (0L < val) {
+      val *= -1;
+    }
+    return -1L + val;
+  }
+
+  /**
+   * 임의의 양수를 반환한다.
+   *
+   * @return 임의의 양수.
+   */
+  public static int positive() {
+    return 1 + R.nextInt(Integer.MAX_VALUE);
+  }
+
+  /**
+   * 임의의 양수를 반환한다.
+   *
+   * @return 임의의 양수.
+   */
+  public static long positiveLong() {
+    long val = R.nextLong();
+    if (Long.MIN_VALUE == val) {
+      return positiveLong();
+    } else if (0L > val) {
+      val *= -1L;
+    }
+    return 1 + val;
+  }
+
+  /**
+   * 0보다 크거나 같은 임의의 수를 반환한다.
+   *
+   * @return 0보다 크거나 같은 임의의 수.
+   */
+  public static int notNegative() {
+    return R.nextInt(Integer.MAX_VALUE);
+  }
+
+  /**
+   * 0보다 크거나 같은 임의의 수를 반환한다.
+   *
+   * @return 0 이상의 임의의 수.
+   */
+  public static long notNegativeLong() {
+    return notNegative(Long.MAX_VALUE);
+  }
+
+  /**
+   * 0 이상, 최대값 미만의 임의의 값을 반환한다.
+   *
+   * @param max
+   *          최대값(미포함).
+   * @return 임의의 0 이상의 수.
+   */
+  public static int notNegative(int max) {
+    Asserts.positive(max);
+    return R.nextInt(max);
+  }
+
+  /**
+   * 0 이상, 최대값 미만의 임의의 값을 반환한다.
+   *
+   * @param max
+   *          최대값(미포함).
+   * @return 임의의 0 이상의 수.
+   *
+   * @see http://stackoverflow.com/a/2546186
+   */
+  public static long notNegative(long max) {
+    Asserts.positive(max);
+    long bits, val;
+    do {
+      bits = R.nextLong() << 1 >>> 1;
+      val = bits % max;
+    } while (bits - val + max - 1 < 0L);
+    return val;
+  }
+
+  /**
+   * 0이 아닌 임의의 수를 반환한다.
+   *
+   * @return 0이 아닌 임의의 수.
+   */
+  public static int notZero() {
+    if (R.nextBoolean()) {
+      return negative();
+    } else {
+      return positive();
+    }
+  }
+
+  /**
+   * 0이 아닌 임의의 수를 반환한다.
+   *
+   * @return 0이 아닌 임의의 수.
+   */
+  public static long notZeroLong() {
+    if (R.nextBoolean()) {
+      return negativeLong();
+    } else {
+      return positiveLong();
+    }
+  }
+
+  /**
+   * 0보다 작거나 같은 임의의 수를 반환한다.
+   *
+   * @return 0 이하의 임의의 수.
+   */
+  public static int notPositive() {
+    return -R.nextInt(Integer.MAX_VALUE);
+  }
+
+  /**
+   * 0보다 작거나 같은 임의의 수를 반환한다.
+   *
+   * @return 0 이하의 임의의 수.
+   */
+  public static long notPositiveLong() {
+    return -notNegative(Long.MAX_VALUE);
+  }
+
+  /**
+   * 최소값과 최대값 사이에서 임의의 수를 반환한다.
+   *
+   * @param min
+   *          최소값(포함).
+   * @param max
+   *          최대값(포함).
+   * @return 임의의 수.
+   * @throws IllegalArgumentException
+   *           임의의 수를 만드는 범위가 <code>int</code>형으로 표현 불가능할 때.
+   */
+  public static int in(int min, int max) throws IllegalArgumentException {
+    Asserts.lt(min, max);
+
+    long width = (long) max - (long) min;
+    if (Integer.MAX_VALUE < width) {
+      return min + (int) notNegative(width);
+    } else {
+      return min + R.nextInt(max - min);
+    }
+  }
+
+  /**
+   * 최소값과 최대값 사이에서 임의의 수를 반환한다.
+   *
+   * @param min
+   *          최소값(포함).
+   * @param max
+   *          최대값(포함).
+   * @return 임의의 수.
+   */
+  public static long in(long min, long max) {
+    Asserts.lt(min, max);
+
+    ConditionalExceptions.positive(max - min, () -> {
+      throw new IllegalArgumentException(format("width[%s] is wider than long max[%d]",
+          BigInteger.valueOf(max).subtract(BigInteger.valueOf(min)), Long.MAX_VALUE));
+    });
+    return min + notNegative(max - min);
+  }
+
+  /**
+   * <code>int</code>형으로 표현 가능한 값 중에서, 최소값과 최대값 사이를 제외한 임의의 수를 반환한다.
+   *
+   * @param min
+   *          최소값(포함).
+   * @param max
+   *          최대값(미포함).
+   * @return 범위 밖의 임의의 수.
+   */
+  public static int notIn(int min, int max) {
+    Asserts.lt(min, max);
+    return R.nextBoolean() ? in(Integer.MIN_VALUE, min + 1) : in(max, Integer.MAX_VALUE);
+  }
+
+  /**
+   * 기준값보다 작은 임의의 수를 반환한다.
+   *
+   * @param boundary
+   *          기준값(미포함).
+   * @return 기준값보다 작은 임의의 수.
+   */
+  public static int lt(int boundary) {
+    if (0 > boundary) {
+      return boundary - 1 - R.nextInt(boundary - Integer.MIN_VALUE);
+    } else {
+      return (int) in((long) Integer.MIN_VALUE, (long) boundary);
+    }
+  }
+
+  /**
+   * 기준값보다 작거나 같은 임의의 수를 반환한다.
+   *
+   * @param boundary
+   *          기준값(미포함).
+   * @return 기준값보다 작거나 같은 임의의 수.
+   */
+  public static int le(int boundary) {
+    if (0 > boundary) {
+      return boundary - R.nextInt(boundary - Integer.MIN_VALUE);
+    } else {
+      return (int) in(Integer.MIN_VALUE, 1L + boundary);
+    }
+  }
+
+  /**
+   * 기준값보다 큰 임의의 수를 반환한다.
+   *
+   * @param boundary
+   *          기준값(미포함).
+   * @return 기준갑보다 큰 임의의 수.
+   */
+  public static int gt(int boundary) {
+    if (0 > boundary) {
+      return boundary + 1 + (int) notNegative((long) Integer.MAX_VALUE - boundary);
+    } else {
+      return boundary + 1 + R.nextInt(Integer.MAX_VALUE - boundary);
+    }
+  }
+
+  /**
+   * 기준값보다 크거나 같은 임의의 수를 반환한다.
+   *
+   * @param boundary
+   *          기준값(미포함).
+   * @return 기준갑보다 크거나 같은 임의의 수.
+   */
+  public static int ge(int boundary) {
+    if (0 > boundary) {
+      return boundary + (int) notNegative((long) Integer.MAX_VALUE - boundary);
+    } else {
+      return boundary + R.nextInt(Integer.MAX_VALUE - boundary);
+    }
+  }
+
+  protected Randoms() {
+    throw new UnsupportedOperationException();
+  }
+}
---
+++ src/test/java/kr/lul/urs/util/RandomsTest.java
@@ -0,0 +1,252 @@
+package kr.lul.urs.util;
+
+import static java.lang.String.format;
+import static kr.lul.urs.util.Randoms.bool;
+import static kr.lul.urs.util.Randoms.ge;
+import static kr.lul.urs.util.Randoms.gt;
+import static kr.lul.urs.util.Randoms.in;
+import static kr.lul.urs.util.Randoms.le;
+import static kr.lul.urs.util.Randoms.lt;
+import static kr.lul.urs.util.Randoms.negative;
+import static kr.lul.urs.util.Randoms.negativeLong;
+import static kr.lul.urs.util.Randoms.notIn;
+import static kr.lul.urs.util.Randoms.notNegative;
+import static kr.lul.urs.util.Randoms.notNegativeLong;
+import static kr.lul.urs.util.Randoms.notPositive;
+import static kr.lul.urs.util.Randoms.notPositiveLong;
+import static kr.lul.urs.util.Randoms.notZero;
+import static kr.lul.urs.util.Randoms.notZeroLong;
+import static kr.lul.urs.util.Randoms.positive;
+import static kr.lul.urs.util.Randoms.positiveLong;
+import static kr.lul.urs.util.Tests.exceptException;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anyOf;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.lessThan;
+import static org.hamcrest.Matchers.lessThanOrEqualTo;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertThat;
+
+import java.math.BigInteger;
+import java.util.Random;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class RandomsTest {
+  private static final int COUNT = Integer.MAX_VALUE / 1024;
+  private Random           rand;
+
+  @Before
+  public void setUp() throws Exception {
+    this.rand = new Random();
+  }
+
+  @Test
+  public void testBool() throws Exception {
+    int t = 0;
+    int f = 0;
+    for (int i = 0; i < COUNT; i++) {
+      if (bool()) {
+        t++;
+      } else {
+        f++;
+      }
+    }
+
+    assertEquals(format("true=%d, false=%d, diff=%d", t, f, Math.abs(t - f)), 0,
+        (int) ((double) Math.abs(t - f) / (COUNT / 2)));
+  }
+
+  @Test
+  public void testNegative() throws Exception {
+    for (int i = 0; i < COUNT; i++) {
+      int val = negative();
+      assertThat(format("i=%d, val=%d", i, val), val, lessThan(0));
+    }
+  }
+
+  @Test
+  public void testNegativeLong() throws Exception {
+    for (int i = 0; i < COUNT; i++) {
+      long val = negativeLong();
+      assertThat(format("i=%d, val=%d", i, val), val, lessThan(0L));
+    }
+  }
+
+  @Test
+  public void testPositive() throws Exception {
+    for (int i = 0; i < COUNT; i++) {
+      int val = positive();
+      assertThat(format("i=%d, val=%d", i, val), val, greaterThan(0));
+    }
+  }
+
+  @Test
+  public void testPositiveLong() throws Exception {
+    for (int i = 0; i < COUNT; i++) {
+      long val = positiveLong();
+      assertThat(format("i=%d, val=%d", i, val), val, greaterThan(0L));
+    }
+  }
+
+  @Test
+  public void testNotNegative() throws Exception {
+    for (int i = 0; i < COUNT; i++) {
+      int number = notNegative();
+      assertThat(format("i=%d, number=%d", i, number), number, greaterThanOrEqualTo(0));
+    }
+  }
+
+  @Test
+  public void testNotNegativeLong() throws Exception {
+    for (int i = 0; i < COUNT; i++) {
+      long number = notNegativeLong();
+      assertThat(format("i=%d, number=%d", i, number), number, greaterThanOrEqualTo(0L));
+    }
+  }
+
+  @Test
+  public void testNotNegativeWithIntMax() throws Exception {
+    for (int i = 0; i < COUNT; i++) {
+      int max = 1 + this.rand.nextInt(Integer.MAX_VALUE);
+      int number = notNegative(max);
+
+      assertThat(number, allOf(greaterThanOrEqualTo(0), lessThan(max)));
+    }
+  }
+
+  @Test
+  public void testNotNegativeWithLongMax() throws Exception {
+    for (int i = 0; i < COUNT; i++) {
+      long max;
+      do {
+        max = this.rand.nextLong();
+      } while (0L >= max);
+      long number = notNegative(max);
+
+      assertThat(number, allOf(greaterThanOrEqualTo(0L), lessThan(max)));
+    }
+  }
+
+  @Test
+  public void testNotZero() throws Exception {
+    for (int i = 0; i < COUNT; i++) {
+      assertNotEquals(0, notZero());
+    }
+  }
+
+  @Test
+  public void testNotZeroLong() throws Exception {
+    for (int i = 0; i < COUNT; i++) {
+      assertNotEquals(0L, notZeroLong());
+    }
+  }
+
+  @Test
+  public void testNotPositive() throws Exception {
+    for (int i = 0; i < COUNT; i++) {
+      assertThat(notPositive(), lessThanOrEqualTo(0));
+    }
+  }
+
+  @Test
+  public void testNotPositiveLong() throws Exception {
+    for (int i = 0; i < COUNT; i++) {
+      assertThat(notPositiveLong(), lessThanOrEqualTo(0L));
+    }
+  }
+
+  @Test
+  public void testInWithInt() throws Exception {
+    for (int i = 0; i < COUNT; i++) {
+      int min = this.rand.nextInt();
+      int max = min + (int) Math.min(Integer.MAX_VALUE, (long) Integer.MAX_VALUE - min);
+      exceptException(AssertionException.class, () -> in(max, min));
+
+      int number = in(min, max);
+      assertThat(format("min=%d, max=%d, number=%d", min, max, number), number,
+          allOf(greaterThanOrEqualTo(min), lessThan(max)));
+    }
+  }
+
+  @Test
+  public void testInWithLong() throws Exception {
+    exceptException(IllegalArgumentException.class, () -> in(-1L, Long.MAX_VALUE));
+
+    BigInteger longMax = BigInteger.valueOf(Long.MAX_VALUE);
+    for (int i = 0; i < COUNT; i++) {
+      long min = this.rand.nextLong();
+      long width;
+      if (0 > longMax.compareTo(longMax.subtract(BigInteger.valueOf(min)))) {
+        width = Long.MAX_VALUE;
+      } else {
+        width = Long.MAX_VALUE - min;
+      }
+      long max = min + 1L + notNegative(width);
+      exceptException(AssertionException.class, () -> in(max, min));
+
+      long number = in(min, max);
+      assertThat(format("min=%d, max=%d, number=%d", min, max, number), number,
+          allOf(greaterThanOrEqualTo(min), lessThan(max)));
+    }
+  }
+
+  @Test
+  public void testNotIn() throws Exception {
+    for (int i = 0; i < COUNT; i++) {
+      int min = this.rand.nextInt();
+      int max = 0 > min ? min + 1 + this.rand.nextInt(Integer.MAX_VALUE)
+          : min + 1 + this.rand.nextInt(Integer.MAX_VALUE - min);
+      exceptException(AssertionException.class, () -> notIn(max, min));
+
+      int number = notIn(min, max);
+      assertThat(number, anyOf(lessThan(min), greaterThanOrEqualTo(max)));
+    }
+  }
+
+  @Test
+  public void testLt() throws Exception {
+    for (int i = 0; i < COUNT; i++) {
+      int boundary = this.rand.nextInt();
+      int number = lt(boundary);
+
+      assertThat(format("number[%d] is not less than boundary[%d].", number, boundary), number, lessThan(boundary));
+    }
+  }
+
+  @Test
+  public void testLe() throws Exception {
+    for (int i = 0; i < COUNT; i++) {
+      int boundary = this.rand.nextInt();
+      int number = le(boundary);
+
+      assertThat(format("number[%d] is not less than or equal to boundary[%d].", number, boundary), number,
+          lessThanOrEqualTo(boundary));
+    }
+  }
+
+  @Test
+  public void testGt() throws Exception {
+    for (int i = 0; i < COUNT; i++) {
+      int boundary = this.rand.nextInt();
+      int number = gt(boundary);
+
+      assertThat(format("number[%d] is not greater than boundary[%d].", number, boundary), number,
+          greaterThan(boundary));
+    }
+  }
+
+  @Test
+  public void testGe() throws Exception {
+    for (int i = 0; i < COUNT; i++) {
+      int boundary = this.rand.nextInt();
+      int number = ge(boundary);
+
+      assertThat(format("number[%d] is not greater than or equal to boundary[%d].", number, boundary), number,
+          greaterThanOrEqualTo(boundary));
+    }
+  }
+}
