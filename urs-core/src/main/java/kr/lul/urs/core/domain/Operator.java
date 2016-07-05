3a290b7637182a3cb82c580410265e0afbf36d92
Just Burrow <just.burrow@lul.kr>
2016-07-05 14:40:04 +0900

    #83: `Operator`에 관한 문서 추가.
    Task-Url: https://github.com/JustBurrow/urs/issues/issue/83

--- urs-core/src/main/java/kr/lul/urs/core/domain/Operator.java
+++ urs-core/src/main/java/kr/lul/urs/core/domain/Operator.java
@@ -10,7 +10,12 @@ import kr.lul.urs.spring.jpa.timestamp.Updatable;
 import kr.lul.urs.util.ToSimpleString;
 
 /**
- * 프로덕트 관리자.
+ * 프로덕트 관리자로, 다음 기능에서 사용한다.
+ * <ul>
+ * <li>프로덕트를 비롯한 데이터를 등록, 사용할 수 있는 소유자.</li>
+ * <li>URS 사용자를 관리하는 단위.</li>
+ * <li>인증 및 데이터 접근 등 권한 확인의 단위.</li>
+ * </ul>
  *
  * @author Just Burrow just.burrow@lul.kr
  */
